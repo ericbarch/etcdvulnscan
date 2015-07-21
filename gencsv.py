e2a2ddcc79909aebef07872cc69668ddc4dc72a5
Eric Barch <ericb@ericbarch.com>
2015-07-20 22:43:54 -0400

    comments

--- gencsv.py
+++ gencsv.py
@@ -1,3 +1,9 @@
+# etcd vulnerability scanner CSV generator
+# v1 (2015-Jul-20)
+# by eric barch (ericbarch.com)
+
+
+# imports
 import csv
 import sqlite3
 import sys
@@ -16,14 +22,18 @@ if not isfile(sqlite_file):
 db = sqlite3.connect(sqlite_file)
 cursor = db.cursor()
 
-
+# select all etcd hosts
 cursor.execute('SELECT ip, port, version, scan_date from etcdscan WHERE etcd_found = 1')   
 
+# open a file for writing the CSV to
 file = open('etcd_hosts.csv', 'w')
 csv_out = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+# create the header
 csv_out.writerow( ('IP', 'Port', 'Version', 'ScanTimestamp') )
 
+# iterate over all the records
 for record in cursor.fetchall():
     csv_out.writerow( record )
 
+# close the file
 file.close()
